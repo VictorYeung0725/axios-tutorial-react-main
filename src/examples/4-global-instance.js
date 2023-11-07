e244f0e9760600d2402eb01f8ed4f10dd858099f
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-11-07 21:04:47 +0800

    adding global default url and custom global setup

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
@@ -6,12 +7,19 @@ const randomUserUrl = 'https://randomuser.me/api';
 const GlobalInstance = () => {
   const fetchData = async () => {
     console.log('global axios instance');
+    try {
+      const reponse1 = await axios(productsUrl);
+      const reponse2 = await axios(randomUserUrl);
+      console.log(reponse2);
+    } catch (error) {
+      console.log(error.reponse);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className="text-center">global instance</h2>;
 };
 export default GlobalInstance;
--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,16 +1,24 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/custom';
+import axios from 'axios';
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
     console.log('custom axios instance');
+    try {
+      const response1 = await authFetch('/react-store-products');
+      const reponse2 = await axios(randomUserUrl);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>custom instance</h2>;
+  return <h2 className="text-center">custom instance</h2>;
 };
 export default CustomInstance;
