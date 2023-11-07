c1026ced8a75e3c2c963c858e1f411ab2e73d5de
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2023-11-07 20:43:48 +0800

    try axios header fetch and post method

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,29 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 // limit, if 429 wait for 15 min and try again
+
+//NOTE axios provide all type of error instead of only netword error in fetch API
+
 const url = 'https://course-api.com/react-store-products';
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      // const response = await axios.get(url);
+      const response = await axios.get(url);
+      const data = response.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   useEffect(() => {
     console.log('first axios request');
+    fetchData();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  return <h2 className="text-center">first request</h2>;
 };
+
 export default FirstRequest;
